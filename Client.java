395451f0bb65e15e288f47f302e30b21207e3595
saintsavon <jaylensavon@gmail.com>
2020-10-23 16:37:06 -0400

    Closed resource leaks

--- Client.java
+++ Client.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Client {
     static String laptopType="";
     public static void main(String[] args) {
-        Client client = new Client();
+        Consultant consultant = new Consultant();
         Builder builder = new Builder();
 
         Scanner scanner = new Scanner(System.in);
@@ -18,6 +18,7 @@ public class Client {
             System.out.println("(3) No thanks, I'm done!");
 
             int a = scanner.nextInt();
+            scanner.close(); // Closing resource leak
             if(a==1) {
                 ArrayList<String> storageList = new ArrayList<>();
                 ArrayList<String> waterCoolList = new ArrayList<>();
@@ -66,6 +67,7 @@ public class Client {
                     System.out.println("(4) I have chosen my storage amount.");
 
                     int value = scan.nextInt();
+                    scan.close();
                     if (value == 1) {
                         storageList.add("500GB");
                         System.out.println("You have chosen a 500GB SSD.");
@@ -91,6 +93,7 @@ public class Client {
                     System.out.println("(4) I would not like water cooling/ have finished selecting my parts.");
 
                     int value = scan2.nextInt();
+                    scan2.close(); // Closing scanner resource leak
                     if (value == 1) {
                         waterCoolList.add("CPUCool");
                         System.out.println("You have chosen to watercool your CPU.");
@@ -119,6 +122,7 @@ public class Client {
                     System.out.println("(5) Razer - $159.99");
                     System.out.println("(6) I would not like a laptop.");
                     int laptop = scanLaptop.nextInt();
+                    scanLaptop.close(); // Closing resource leaks
                     if(laptop==1){
                         laptopType="Lenovo";
                         System.out.println("You have chosen the Lenovo laptop.");
